e4c6d185d52c13511612df46db4a7bc2edaf0d77
MEZLYSO <lynuxazura@gmail.com>
2025-07-19 12:52:07 -0600

    Seed Implement

--- pokedex/src/pokemon/pokemon.module.ts
+++ pokedex/src/pokemon/pokemon.module.ts
@@ -14,6 +14,7 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
       //El esquema que seguira mongo
       schema: PokemonSchema
     }])
-  ]
+  ],
+  exports: [MongooseModule]
 })
 export class PokemonModule { }
--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interface/poke-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
 
   // Crear instacnia de Axios 
   // Forma erronea debido a que es una dependencia
@@ -18,9 +27,9 @@ export class SeedService {
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/')
       const no: number = +segments[segments.length - 2]
+      const pokemon = this.pokemonModel.create({ name, no })
     })
-
-    return data.results
+    return { message: "Sucess" }
   }
 
 }
